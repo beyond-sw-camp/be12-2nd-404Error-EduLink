f4fcd708abc2df87910e5c8ae639e836b45dc80d
goodleaf96 <ky1234tho@gmail.com>
2025-03-07 10:34:31 +0900

    [feat] 네비게이션 바에 사용자 이름이 출력되도록 변경

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import router from './router'
 import axios from 'axios';
 import 'flowbite';
@@ -12,6 +13,8 @@ axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.withCredentials = true;
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
 const app = createApp(App);
 app.use(pinia);
 app.use(router)
