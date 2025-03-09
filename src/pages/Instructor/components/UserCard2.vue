ad9d7b2c999a3d7fb5856a1570521f1c9b5ee8ca
museongkim <mstar0228@gmail.com>
2025-03-09 13:49:41 +0900

    [Fix] 학생 카드 이미지 변경 및 드롭다운 메뉴 제거거

--- src/pages/Instructor/components/UserCard2.vue
+++ src/pages/Instructor/components/UserCard2.vue
@@ -14,7 +14,7 @@ const props = defineProps({
             <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
                 <span class="sr-only">Open dropdown</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                    <!-- <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/> -->
                 </svg>
             </button>
             <!-- Dropdown menu -->
@@ -33,7 +33,7 @@ const props = defineProps({
             </div>
         </div>
         <div class="flex flex-col items-center pb-10">
-            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../../assets/vue.svg" alt="Bonnie image"/>
+            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://api.dicebear.com/7.x/avataaars/svg?seed=doe-doe-doe-example-com" alt="Bonnie image"/>
             <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white"> {{ student.name }}</h5>
             <span class="text-sm text-gray-500 dark:text-gray-400">{{ student.email }}</span>
             <span class="text-sm text-gray-500 dark:text-gray-400">{{ student.studentDetail.address }}</span>
